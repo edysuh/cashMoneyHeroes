71b4d027c84e1e26f16f29c50d7c4db213afd7fd
chunankong <chenxu2018@u.northwestern.edu>
2017-02-08 22:02:11 -0600

    added details for each category

--- home.js
+++ home.js
@@ -9,6 +9,16 @@ function calcSubtotalWidth(cat) {
 	cat_sub_bar.style.width = cat_sub_value / totalvalue * 100 + '%';
 }
 
+function calcCatWidth(cat,catTot) {
+	const totalbar = document.getElementById(catTot);
+	const cat_sub_bar = document.getElementById(cat);
+	var totalvalue = totalbar.getAttribute("value");
+	var cat_sub_value = cat_sub_bar.getAttribute("value");
+	
+	cat_sub_bar.style.width = cat_sub_value / totalvalue * 100 + '%';
+}
+
+
 calcSubtotalWidth("food");
 calcSubtotalWidth("coffee");
 calcSubtotalWidth("clothes");
@@ -16,4 +26,8 @@ calcSubtotalWidth("alcohol");
 calcSubtotalWidth("textbooks");
 
 
-
+calcCatWidth("catbarfood","catFood");
+calcCatWidth("catbarcoffee","catCoffee");
+calcCatWidth("catbarclothes","catClothes");
+calcCatWidth("catbaralcohol","catAlcohol");
+calcCatWidth("catbartextbook","catTextbook");
