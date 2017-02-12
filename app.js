dae803116a729f502685b659265426fdc2876036
edysuh <edys@u.northwestern.edu>
2017-02-12 15:12:38 -0600

    nothing

--- app.js
+++ app.js
@@ -11,32 +11,32 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/views/home.html");
 });
 
-// ---- testing ----
-
-const Category = require(__dirname + '/models/categories.js');
-
-var food = new Category({
-	name: 'food',
-	budget: 400,
-	spent: 280,
-});
-console.log('food', food);
-
-Category.find({ name: 'food'}, function(err, cat) {
-	if (err) throw err;
-	
-	food.setBudget(500);
-	food.save((err) => {
-		if (err) throw err;
-		console.log("save!");
-	});
-	console.log(cat);
-});
-
-console.log('food', food);
-
-// ---- testing end ----
-
 app.listen(9000, () => {
 	console.log("app.js listening on port 9000");
 });
+
+// ---- testing ----
+
+// const Category = require(__dirname + '/models/categories.js');
+
+// var food = new Category({
+// 	name: 'food',
+// 	budget: 400,
+// 	spent: 280,
+// });
+// console.log('food', food);
+
+// Category.find({ name: 'food'}, function(err, cat) {
+// 	if (err) throw err;
+	
+// 	food.setBudget(500);
+// 	food.save((err) => {
+// 		if (err) throw err;
+// 		console.log("save!");
+// 	});
+// 	console.log(cat);
+// });
+
+// console.log('food', food);
+
+// ---- testing end ----
