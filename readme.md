0e9a9708d13792e3e2be0a87e70e5548cdcbb0d4
Jeff Yang <Jeff@dhcp-10-105-123-134.wireless.northwestern.private>
2017-02-24 10:34:28 -0600

    landing/login page

--- readme.md
+++ readme.md
@@ -1,17 +1,14 @@
 cashMoneyHeroes is a money budgeting app for college students
 
-### how to install
+### how to install on Mac OS X
 
 1. install brew from http://brew.sh/
 2. install the latest node.js with the command:
 	`brew install node`
-3. install mongodb with:
-	`brew install mongodb`
-	**** WE CAN IGNORE STEP 3 FOR NOW ****
-4. in this directory, run `npm install`
-5. run `node server.js` or `npm start` to launch the app
+3. in this directory, run `npm install`
+4. run `node server.js` or `npm start` to launch the app 
 	- however, i recommend using `nodemon` to run the app; itll let to you refresh the app on save,
 		without having to restart the app everytime
-		- run `npm install nodemon`
-		- then use `./node_modules/.bin/nodemon server.js` instead to launch the app
-	**** ACTUALLY DONT DO THIS RIGHT NOW ITS BROKEN ****
+		- run `npm install --save-dev nodemon`
+		- then use `npm run nodemon` instead to launch the app
+5. open the web app in your browser at localhost:9000
--- server.js
+++ server.js
@@ -2,26 +2,84 @@ var express = require('express'),
 	app = express(),
 	path = require('path'),
 	fs = require('fs'),
-	filename = './data/data.json',
+	exphbs = require('express-handlebars'),
+	bodyParser = require('body-parser');
+
+var filename = './data/data.json',
 	data = require(filename);
+
+// configure application ----
+
+app.set('port', (process.env.PORT || 9000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/html/index.html'));
+var handlebars = exphbs.create({
+	defaultLayout: 'main',
+	extname: 'html',
 });
 
-function modifyBudget(category, val) {
-	data.categories[category].budget = val;
-	console.log('data', data);
+app.engine('html', handlebars.engine);
+app.set('view engine', 'html');
 
-	fs.writeFileSync(filename, JSON.stringify(data, null, 2), (err) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// rendering views ----
+
+app.get('/', (req, res) => {
+	res.render('home', data);
+});
+
+app.get('/budget', (req, res) => {
+	res.render('budget', data);
+});
+
+// post requests ----
+
+app.post('/budget', (req, res) => {
+	var budget_data = req.body;
+	var total_budget = 0;
+	
+	for (var key in budget_data) {
+		var category = key.split("_")[0];
+		var category_budget = budget_data[key];
+		total_budget += parseInt(category_budget);
+		
+		modifyBudget(category, category_budget);
+	}
+	
+	data.total.budget = makeStringMoney(String(total_budget));
+
+	res.render('budget', data);
+});
+
+// start the app ----
+
+app.listen(app.get('port'), () => {
+	console.log('-------- listening on port', app.get('port'), 'for requests to re:server --------');
+});
+
+// helper functions ----
+
+function modifyBudget(category, str_val) {
+	data.categories[category].budget = makeStringMoney(str_val);
+
+	fs.writeFile(filename, JSON.stringify(data, null, 2), (err) => {
 		if (err) console.log(err);
 	});
 }
 
-modifyBudget("food", 10);
-
-app.listen(9000, () => {
-	console.log('-------- listening on port 9000 for requests to re:server --------');
-});
+function makeStringMoney(str_val) {
+	str_val = str_val.split('.');
+	
+	if (!str_val[1]) {
+		str_val = str_val[0] + '.00';
+	} else if (str_val[1].length !== 2) {
+		str_val = str_val[0] + '.' + str_val[1] + '0';
+	} else {
+		str_val = str_val[0] + '.' + str_val[1];
+	}
+	
+	return str_val;
+}
